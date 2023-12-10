2eb3c2edaa17512b04cab01f3dea1d272191ec0d
ImSpiDy <SpiDy2713@gmail.com>
2023-12-10 13:14:12 +0000

    lavender: Bringup for xDroid Android 11
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,13 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lavender.mk
+    $(LOCAL_DIR)/lineage_lavender.mk \
+    $(LOCAL_DIR)/xdroid_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_lavender-user \
     lineage_lavender-userdebug \
-    lineage_lavender-eng
+    lineage_lavender-eng \
+    xdroid_lavender-eng \
+    xdroid_lavender-userdebug \
+    xdroid_lavender-user
---
+++ xdroid_lavender.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit some common XDroid stuff
+$(call inherit-product, vendor/xdroid/config/common.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Inherit from lavender device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := lavender
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_NAME := xdroid_lavender
+PRODUCT_MODEL := Redmi Note 7
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+TARGET_VENDOR_PRODUCT_NAME := lavender
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="lavender-user 10 QKQ1.190910.002 V12.5.3.0.QFGCNXM release-keys"
+
+BUILD_FINGERPRINT := xiaomi/lavender/lavender:10/QKQ1.190910.002/V12.5.3.0.QFGCNXM:user/release-keys
