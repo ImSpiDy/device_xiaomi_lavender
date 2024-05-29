LineageOS/android_device_xiaomi_lavender

lavender: Enforce common device & vendor tree presence
A compiled build has no chance of booting without the common device setup
and the prebuilt binaries, so fail the build if those are not present.

Change-Id: Iadd43e887d426732e549969ab8deb14194ca818d

device.mk
@@ -68,5 +68,5 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     LavenderWifiOverlay
 
-# Inherit proprietary files
-$(call inherit-product-if-exists, vendor/xiaomi/lavender/lavender-vendor.mk)
+# Inherit the proprietary files
+$(call inherit-product, vendor/xiaomi/wayne/wayne-vendor.mk)

ImSpiDy/device_xiaomi_lavender

device.mk
@@ -64,5 +64,5 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
-# Inherit proprietary files
-$(call inherit-product-if-exists, vendor/xiaomi/lavender/lavender-vendor.mk)
+# Inherit the proprietary files
+$(call inherit-product, vendor/xiaomi/lavender/lavender-vendor.mk)