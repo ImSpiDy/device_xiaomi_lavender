wHo-EM-i/device_xiaomi_lavender-old

lavender: switch to retrofit dynamic partitioning
Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

BoardConfig.mk
@@ -30,6 +30,7 @@ TARGET_SCREEN_DENSITY := 420
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # Kernel
+BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/c0c4000.sdhci
 TARGET_KERNEL_CONFIG := lavender_defconfig
 BOARD_KERNEL_SEPARATED_DTBO := true
 
@@ -37,9 +38,30 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Partitions
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3640619008
-BOARD_VENDORIMAGE_PARTITION_SIZE := 2080305152
+#BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3758096384
+#BOARD_VENDORIMAGE_PARTITION_SIZE := 2147483648
 BOARD_DTBOIMG_PARTITION_SIZE := 8388608
+
+BOARD_SUPER_PARTITION_GROUPS := qti_dynamic_partitions
+BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := odm product system system_ext vendor
+BOARD_ODMIMAGE_FILE_SYSTEM_TYPE := erofs
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := erofs
+BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := erofs
+BOARD_SUPER_PARTITION_METADATA_DEVICE := system
+BOARD_SUPER_PARTITION_BLOCK_DEVICES := system vendor
+BOARD_SUPER_PARTITION_SYSTEM_DEVICE_SIZE := 3758096384
+BOARD_SUPER_PARTITION_VENDOR_DEVICE_SIZE := 2147483648
+BOARD_SUPER_PARTITION_SIZE := 5720924160
+BOARD_QTI_DYNAMIC_PARTITIONS_SIZE := 5901385728 # (BOARD_SUPER_PARTITION_SIZE - 4194304) 4MiB overhead
+
+BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 614400000
+BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 92160000
+BOARD_SYSTEM_EXTIMAGE_PARTITION_RESERVED_SIZE := 92160000
+BOARD_ODMIMAGE_PARTITION_RESERVED_SIZE := 104857600
+
+TARGET_COPY_OUT_ODM := odm
+TARGET_COPY_OUT_PRODUCT := product
+TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
@@ -49,6 +71,9 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
 # Security patch level
 VENDOR_SECURITY_PATCH := 2021-06-01
+
+# SELinux
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Inherit the proprietary files
 include vendor/xiaomi/lavender/BoardConfigVendor.mk

ImSpiDy/device_xiaomi_lavender

BoardConfig.mk
@@ -25,6 +25,7 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_DENSITY := 420
 
 # Kernel
+BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/c0c4000.sdhci
 TARGET_KERNEL_CONFIG := lavender_defconfig
 BOARD_KERNEL_SEPARATED_DTBO := true
 
@@ -32,9 +33,30 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Partitions
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3640619008
-BOARD_VENDORIMAGE_PARTITION_SIZE := 2080305152
+#BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3758096384
+#BOARD_VENDORIMAGE_PARTITION_SIZE := 2147483648
 BOARD_DTBOIMG_PARTITION_SIZE := 8388608
+
+BOARD_SUPER_PARTITION_GROUPS := qti_dynamic_partitions
+BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := odm product system system_ext vendor
+BOARD_ODMIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SUPER_PARTITION_METADATA_DEVICE := system
+BOARD_SUPER_PARTITION_BLOCK_DEVICES := system vendor
+BOARD_SUPER_PARTITION_SYSTEM_DEVICE_SIZE := 3758096384
+BOARD_SUPER_PARTITION_VENDOR_DEVICE_SIZE := 2147483648
+BOARD_SUPER_PARTITION_SIZE := 5720924160
+BOARD_QTI_DYNAMIC_PARTITIONS_SIZE := 5901385728 # (BOARD_SUPER_PARTITION_SIZE - 4194304) 4MiB overhead
+
+BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 614400000
+BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 92160000
+BOARD_SYSTEM_EXTIMAGE_PARTITION_RESERVED_SIZE := 92160000
+BOARD_ODMIMAGE_PARTITION_RESERVED_SIZE := 104857600
+
+TARGET_COPY_OUT_ODM := odm
+TARGET_COPY_OUT_PRODUCT := product
+TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
@@ -47,6 +69,9 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
 # Security patch level
 VENDOR_SECURITY_PATCH := 2021-06-01
+
+# SELinux
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Inherit the proprietary files
 include vendor/xiaomi/lavender/BoardConfigVendor.mk