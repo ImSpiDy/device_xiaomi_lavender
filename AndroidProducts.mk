45ce29849f7ab2bedda8b9b7159c90a2fc332fb4
ImSpiDy <SpiDy2713@gmail.com>
2026-06-30 04:44:05 +0000

    lavender: Add PixelOS-AOSP
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,6 +6,7 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_lavender.mk \
+    $(LOCAL_DIR)/custom_lavender.mk \
     $(LOCAL_DIR)/infinity_lavender.mk \
     $(LOCAL_DIR)/lineage_lavender.mk \
     $(LOCAL_DIR)/pixelage_lavender.mk
